b14c424a2893aabe643ec30ca66e5207d59ac8fd
ShadowMonarch <72171234+shadow-monarch-solo@users.noreply.github.com>
2021-10-31 21:50:24 +0530

    Add files via upload

---
+++ March_Lunchtime1.py
@@ -0,0 +1,6 @@
+for t in range(int(input())):
+    a,y,x=map(int,input().split())
+    #a - time of arrival of gf
+    #y - time after which candel is extinguished
+    #x - number of candels
+    print(x)
